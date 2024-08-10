3cc8cbb27bf4b5872112ddc4d7fcc3f32198331c
Aswanth C Manoj <aswanthmanoj51@gmail.com>
2024-08-10 17:24:27 +0530

    Update __init__.py

--- fluidframe/utilities/__init__.py
+++ fluidframe/utilities/__init__.py
@@ -3,10 +3,16 @@ from typing import Optional, List
 from fluidframe.core import span, script
 
 def add_tooltip(id: str, message: str, cls: str) -> str:
-    return ''.join([span(message, cls=f"{cls} tooltip-content", tooltip_target=f"#{id}"), script(f"FluidFrameTooltip.init(document.getElementById('{id}'));")])
+    return span(message, id=f"tooltip-{id}", cls=f"{cls}") # , script(f"FluidFrameTooltip.init(document.getElementById('{id}'));")])
+
+def show_tooltip(id: str) -> str:
+    return f"showTooltip('{id}', 'tooltip-{id}')"
+    
+def hide_tooltip(id: str) -> str:
+    return f"hideTooltip('tooltip-{id}')"
 
 def copy_code(id: str) -> str:
-    return ''.join(["copyCodeToClipboard('", id, "');"])
+    return f"copyCodeToClipboard('{id}');"
 
 def highlight_code(id: str, language: Optional[str]=None, theme: Optional[str]="github-dark", show_line_number: bool=False) -> str:
     pass
@@ -14,4 +20,4 @@ def highlight_code(id: str, language: Optional[str]=None, theme: Optional[str]="
 def requires(scripts: Optional[List[str]]=None, styles: Optional[List[str]]=None) -> str:
     styles = [] if styles is None else styles
     scripts = [] if scripts is None else scripts
-    return script(''.join(["loadDependencies({", "scripts:", json.dumps(scripts), ", styles:", json.dumps(styles), "});"]))
+    return script(f"loadDependencies({{scripts: {json.dumps(scripts)}, styles: {json.dumps(styles)}}});")
